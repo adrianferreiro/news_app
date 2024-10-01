50535f72e6e2039bb280ee43d772d898e3063a59
Adrian Ferreiro <adriferreiro.88@gmail.com>
2024-10-01 10:23:18 -0300

    Create files article
    *1:	capa dominio: definimos article *2:	capa dominio: definimos repository, clases abstractas para saber cuales son las implementaciones que tenemos que hacer en ‘models->repository’ *3:	antes de implementar en el dominio tenemos que crear el modelo en ‘data->models’: ArticleModel extendemos de ArticleEntity y creamos el fromJson *4: implementar el repositorio de artículos en ‘data/repository’: article_repository_impl.dart que extiende de ArticleRepository.dart *5: data_sources/remote/news_api_service.dart para consumir la api en remoto, después creamos ‘local’ -5.1: creamos un archivo “constants” en core para la url de la api -5.2: utilizamos la constante en news_api_service.dart *6: creamos usecases: core->asechases-usecase.dart   -6.1: creamos usecases en domain->usecases->get_article.dart

---
+++ lib/features/daily_news/domain/entities/article.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class ArticleEntity extends Equatable {
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publichedAt;
+  final String? content;
+
+  const ArticleEntity({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publichedAt,
+    this.content,
+  });
+
+  @override
+  //para cumplir el contrato de equatable
+  List<Object?> get props {
+    return [
+      id,
+      author,
+      title,
+      description,
+      url,
+      urlToImage,
+      publichedAt,
+      content,
+    ];
+  }
+}
